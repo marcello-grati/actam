63f5da7614954a6c3554e339adc01af8b057b65b
Alessandra Moro <79703541+ale-moro@users.noreply.github.com>
2022-11-15 09:57:33 +0100

    creazione array con elementi selezionati

--- Files/avatarpage_app.js
+++ Files/avatarpage_app.js
@@ -1,6 +1,7 @@
 const selectionMenu = document.getElementById("selection-menu");
 const optionsContainer = document.getElementById("options-container");
 const nameAvatar = document.getElementById("name-avatar");
+const selectedElement = document.getElementsByClassName("options-container");
 
 function saveName() {
 
@@ -14,18 +15,25 @@ function saveName() {
 }
 
 const options = {
-    "skin": ['color1', 'color2', 'color3', 'color4', 'color5', 'color6', 'color7', 'color8', 'color9', 'color10'],
-    "haircolor": ['color1', 'color2', 'color3', 'color4', 'color5', 'color6', 'color7', 'color8', 'color9', 'color10', 'color11', 'color12'],
-    "haircut": ['1','2','3'],
-    "eyes": ['color1', 'color2', 'color3', 'color4'],
-    "nose":['type1', 'type2', 'type3', 'type4'],
-    "mouth": ['type1','type2']
+    "skin": ['color1s', 'color2s', 'color3s', 'color4s', 'color5s', 'color6s', 'color7s', 'color8s', 'color9s', 'color10s'],
+    "haircolor": ['color1h', 'color2h', 'color3h', 'color4h', 'color5h', 'color6h', 'color7h', 'color8h', 'color9h', 'color10h', 'color11h', 'color12h'],
+    "haircut": ['1h','2h','3h'],
+    "eyes": ['color1e', 'color2e', 'color3e', 'color4e'],
+    "nose":['type1n', 'type2n', 'type3n', 'type4n'],
+    "mouth": ['type1m','type2m']
 
 };
+
+avatarFeat = [0,0,0,0,0,0];
+
 
 selectionMenu.addEventListener("change", function() {
     const selected = selectionMenu.value;
     const filenames = options[selected];
+    const selected_el = e => {
+        arrayCreation(e.target.id, selected)
+    }
+
 
     optionsContainer.replaceChildren([]);
 
@@ -33,8 +41,39 @@ selectionMenu.addEventListener("change", function() {
         const el_img = document.createElement('div');
         el_img.classList.add('neonText');
         el_img.innerText = filename;
+        el_img.setAttribute('id',filename)
+        el_img.addEventListener("click", selected_el)
 
         optionsContainer.appendChild(el_img);
     }
+
+
 })
+
+function arrayCreation(elem, selected){
+    // [0]=hair color, [1]=haircut, [2]=eyes, [3]=skin [4]=nose, [5]=mouth
+    if(selected==='haircolor')
+        avatarFeat[0] = elem
+    else if(selected==='haircut')
+        avatarFeat[1] = elem
+    else if(selected==='eyes')
+        avatarFeat[2] = elem
+    else if(selected==='skin')
+        avatarFeat[3] = elem
+    else if(selected==='nose')
+        avatarFeat[4] = elem
+    else if(selected==='mouth')
+        avatarFeat[5] = elem
+
+    // Per visualizzare array in console
+    console.log(avatarFeat[0]+','+avatarFeat[1]+','+avatarFeat[2]+','+avatarFeat[3]+','+avatarFeat[4]+','+avatarFeat[5])
+
+}
+
+
+
+
+
+
+
 
