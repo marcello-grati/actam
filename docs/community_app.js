ca84e918502c80bcf7783698ac0d939e0eef4d63
silviapasin <silvia.pasin@mail.polimi.it>
2023-04-04 13:54:01 +0200

    corretti alcuni bug

--- docs/community_app.js
+++ docs/community_app.js
@@ -39,7 +39,7 @@ const options = {
         'chocolate_hair',
         'gold_hair',
     ],
-    haircut: ['1h', '2h', '3h'],
+    haircut: ['0h', '1h', '2h', '3h'],
     eyes: ['green_eyes', 'blue_eyes', 'brown_eyes', 'grey_eyes'],
     nose: ['type1n', 'type2n', 'type3n', 'type4n'],
     mouth: ['type1m', 'type2m'],
